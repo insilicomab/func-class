7c36a89b93648da93abfd61c3520b398c7493ce8
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-05-07 10:57:16 +0900

    add file

---
+++ class/data_fields.py
@@ -0,0 +1,13 @@
+# リスト11.29
+import dataclasses
+
+
+@dataclasses.dataclass()
+class Person:
+    firstname: str
+    lastname: str
+
+
+if __name__ == "__main__":
+    for f in dataclasses.fields(Person):
+        print(f)
